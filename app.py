fbc30047a72e0218b255a0f1128abef7e60b659f
Om9523 <158480234+Om9523@users.noreply.github.com>
2025-06-12 00:55:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from pymongo import MongoClient
+from flask_pymongo import PyMongo 
 from bson.objectid import ObjectId
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
